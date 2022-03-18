897c7ab408554aa2ad7503dabbf21088b030d3d2
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2022-03-19 01:26:11 +0200

    Add files via upload

---
+++ Basics Online Exam - 6 and 7 July 2019/2.FamiliTrip/2.FamiliTrip.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+	var budget, nights, price, percent, subtotal float64
+	fmt.Scan(&budget, &nights, &price, &percent)
+
+	if nights > 7 {
+		subtotal = (price * 0.95) * nights
+	} else {
+		subtotal = (price) * nights
+	}
+
+	discount := budget * percent / 100
+	total := subtotal + discount
+	finalSum := budget - total
+
+	if finalSum < 0 {
+		fmt.Printf("%.2f leva needed.", math.Abs(finalSum))
+	} else {
+		fmt.Printf("Ivanovi will be left with %.2f leva after vacation.", finalSum)
+	}
+}
+
+/*
+name   :02. Family Trip
+input  :800.50 8 100 2
+output :Ivanovi will be left with 24.49 leva after vacation.
+*/
